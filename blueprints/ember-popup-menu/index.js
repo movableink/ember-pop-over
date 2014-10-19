0c8c0fc2f4fb92be7535464ed75520fd690b5792
Tim Evans <tim-evans@sproutcore.com>
2014-10-18 21:04:16 -0400

    add blueprints for adding dom-ruler

---
+++ blueprints/ember-popup-menu/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  normalizeEntityName: function() {},
+  afterInstall: function() {
+    return this.addBowerPackageToProject('dom-ruler#0.1.2');
+  }
+};
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 module.exports = {
-  name: 'ember-popup-menu'
+  name: 'ember-popup-menu',
+  included: function (app) {
+    this._super.included(app);
+    app.import('bower_components/dom-ruler/dist/dom-ruler.amd.js', {
+      exports: {
+        'dom-ruler': ['default']
+      }
+    });
+  }
 };
